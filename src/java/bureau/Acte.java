aa20cfce26c12c4d195c274e830a2a735534897b
antoinelapeyreunivjfc <Skip@MacBook-Pro-de-Skip.local>
2016-09-21 12:15:59 +0200

    Modification table Acte

--- src/java/bureau/Acte.java
+++ src/java/bureau/Acte.java
@@ -53,5 +53,71 @@ public class Acte implements Serializable {
     @Column
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
     List<Image> images;
+
+    public int getId_acte() {
+        return id_acte;
+    }
+
+    public void setId_acte(int id_acte) {
+        this.id_acte = id_acte;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getHeure() {
+        return heure;
+    }
+
+    public void setHeure(Date heure) {
+        this.heure = heure;
+    }
+
+    public String getIEP() {
+        return IEP;
+    }
+
+    public void setIEP(String IEP) {
+        this.IEP = IEP;
+    }
+
+    public String getCode_UF() {
+        return code_UF;
+    }
+
+    public void setCode_UF(String code_UF) {
+        this.code_UF = code_UF;
+    }
+
+    public String getId_modalite() {
+        return id_modalite;
+    }
+
+    public void setId_modalite(String id_modalite) {
+        this.id_modalite = id_modalite;
+    }
+
+    public String getCode_CCAM() {
+        return code_CCAM;
+    }
+
+    public void setCode_CCAM(String code_CCAM) {
+        this.code_CCAM = code_CCAM;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
+    
+    
     
 }
