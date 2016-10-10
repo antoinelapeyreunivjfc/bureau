2fde1ff3b93a084d6b86c2640779c42952b70620
antoinelapeyreunivjfc <Skip@MacBook-Pro-de-Skip.local>
2016-10-10 10:10:39 +0200

    Ajout de Admission et Images
    Ajout des classes Admission et Images

---
+++ src/java/bureau/Admission.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bureau;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Charline
+ */
+public class Admission implements Serializable {
+    
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private String IEP;
+   
+ @Column
+ private String nom_patient;
+ 
+ private String prenom_patient;
+ private String IPP_Patient;
+ 
+List liste_actes=new ArrayList() ;
+
+    public Admission(String IEP, String nom_patient, String prenom_patient, String IPP_Patient) {
+        this.IEP = IEP;
+        this.nom_patient = nom_patient;
+        this.prenom_patient = prenom_patient;
+        this.IPP_Patient = IPP_Patient;
+    }
+
+    public String getIEP() {
+        return IEP;
+    }
+
+    public String getNom_patient() {
+        return nom_patient;
+    }
+
+    public String getPrenom_patient() {
+        return prenom_patient;
+    }
+
+    public String getIPP_Patient() {
+        return IPP_Patient;
+    }
+
+    public List getListe_actes() {
+        return liste_actes;
+    }
+
+    public void setIEP(String IEP) {
+        this.IEP = IEP;
+    }
+
+    public void setNom_patient(String nom_patient) {
+        this.nom_patient = nom_patient;
+    }
+
+    public void setPrenom_patient(String prenom_patient) {
+        this.prenom_patient = prenom_patient;
+    }
+
+    public void setIPP_Patient(String IPP_Patient) {
+        this.IPP_Patient = IPP_Patient;
+    }
+
+    public void setListe_actes(List liste_actes) {
+        this.liste_actes = liste_actes;
+    }
+
+
+}
---
+++ src/java/bureau/Images.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bureau;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Charline
+ */
+public class Images {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id_image;
+    
+   @Column
+   private String file_image;
+
+    public Images(String id_image, String file_image) {
+        this.id_image = id_image;
+        this.file_image = file_image;
+    }
+
+    public String getId_image() {
+        return id_image;
+    }
+
+    public String getFile_image() {
+        return file_image;
+    }
+
+    public void setId_image(String id_image) {
+        this.id_image = id_image;
+    }
+
+    public void setFile_image(String file_image) {
+        this.file_image = file_image;
+    }
+   
+   
+}
